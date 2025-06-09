b08850069c786e7e14f9965c1c44303a9f824f01
Stan Choi <sjc591@nyu.edu>
2025-06-08 20:55:45 -0400

    Accept cli

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,7 +6,13 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-text_response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+sys_args = sys.argv
+if len(sys_args) == 1:
+    # prompt is not provided, so print err message and exit program with exit code 1
+    print("ERROR: prompt is not provided")
+    exit(1)
+    
+text_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys_args[1])
 
 print(text_response)
 print(f"Prompt tokens: {text_response.usage_metadata.prompt_token_count}")
