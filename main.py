c0be34a4352923a55494dac37d5d3135246413c5
Stan Choi <sjc591@nyu.edu>
2025-06-08 20:59:10 -0400

    Refactor

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-import os, sys
+import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
 
-client = genai.Client(api_key=api_key)
-sys_args = sys.argv
-if len(sys_args) == 1:
-    # prompt is not provided, so print err message and exit program with exit code 1
-    print("ERROR: prompt is not provided")
-    exit(1)
-    
-text_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys_args[1])
+    args = sys.argv[1:]
 
-print(text_response)
-print(f"Prompt tokens: {text_response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {text_response.usage_metadata.candidates_token_count}")
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python man.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    text_response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+
+    print(f"Prompt tokens: {text_response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {text_response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(text_response.text)
+
+if __name__ == "__main__":
+    main()
